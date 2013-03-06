8c2c76c189e3d7840d61a2a814365f14ece78583
unknown <r216-stu@20024-0148590.aipdcls.edu>
2013-03-05 17:03:55 -0800

    adding to github

---
+++ Marcus_Carter_PhpFinal/head.php
@@ -0,0 +1,13 @@
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title>PHP Final</title>
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/foundation.css">
+
+  <script src="js/vendor/custom.modernizr.js"></script>
+</head>
---
+++ Marcus_Carter_PhpFinal/index.php
@@ -0,0 +1,259 @@
+
+<!DOCTYPE html>
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title>PHP Final</title>
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/foundation.css">
+
+  <script src="js/vendor/custom.modernizr.js"></script>
+</head>
+<body>
+
+  <nav class="top-bar">
+
+    <ul class="title-area">
+      <a href="index.html">Marcus Carter Design</a>
+      <li class="name">
+        <h1>
+          <a href="#">
+            
+          </a>
+        </h1>
+      </li>
+      <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
+    </ul>
+ 
+    <section class="top-bar-section">
+      <!-- Right Nav Section -->
+      <ul class="right">
+        <li class="divider"></li>
+        <li class="has-dropdown">
+          <a href="#">Projects</a>
+          <ul class="dropdown">
+            <li><label>Websites</label></li>
+            <li><a href="project1.html">Lebron Fashion</a></li>
+            <li><a href="project2.html">Miami Heat</a></li>
+            <li><a href="project3.html">Steve Jobs Autobiography</a></li>
+            <li><a href="#">Pittock Mansion</a></li>
+            <li><a href="#">Portland Art Museum</a></li>
+            <li class="divider"></li>
+			<li><label>Cultural</label></li>
+            <li><a href="#">Japanese Gardens</a></li>
+            <li><a href="#">Lan Su Chinese Garden</a></li>
+            <li class="divider"></li>
+			<li><label>Unique to Portland</label></li>
+            <li><a href="#">Saturday Market</a></li>
+            <li><a href="#">Voodoo Donuts</a></li>
+            <li class="divider"></li>
+            
+          </ul>
+        </li>
+        <li class="divider"></li>
+        <li><a href="resume.html">Resume</a></li>
+        <li class="divider"></li>
+        <li><a href="contact.html">Contact</a>
+        </li>
+      </ul>
+    </section>
+  </nav>
+ 
+ 
+  <!-- End Top Bar -->
+
+  <div class="row">
+    <div class="large-12 columns">
+
+    <!-- Content Slider -->
+	  <a name="top"></a>
+      <div class="row">
+        <div class="large-12 hide-for-small">
+
+          <div id="featured" data-orbit>
+              <a class="slides" href="project1.html"><img src="img/lbj.png" alt="Lebron James Fashion" title="lbj"></a>
+			   <a class="slides" href="project2.html"><img src="img/miami.png" alt="Miami Heat Business" title="Heat"></a>
+			    <a class="slides" href="project3.html"><img src="img/steve.png" alt="Steve Jobs" title="Jobs"></a>
+			 
+              
+            </div>
+
+      </div>
+    </div>
+	 <div class="hr"><a name="zoo">&nbsp;</a></div>
+
+    <!-- End Content Slider -->
+
+    <!-- Mobile Header -->
+
+      <div class="row">
+        <div class="large-12 columns show-for-small">
+
+           <img src="img/mobile_bg.jpg" alt="Portland Sign" title="Sign">
+		   
+
+        </div>
+      </div>
+
+    <!-- End Mobile Header -->
+
+	
+      <!-- Thumbnails -->
+ 
+        <div class="large-3 small-6 columns">
+          <img src="http://placehold.it/250x250&text=Thumbnail" />
+          <h6 class="panel">Description</h6>
+        </div>
+ 
+        <div class="large-3 small-6 columns">
+          <img src="http://placehold.it/250x250&text=Thumbnail" />
+          <h6 class="panel">Description</h6>
+        </div>
+ 
+        <div class="large-3 small-6 columns">
+          <img src="http://placehold.it/250x250&text=Thumbnail" />
+          <h6 class="panel">Description</h6>
+        </div>
+ 
+        <div class="large-3 small-6 columns">
+          <img src="http://placehold.it/250x250&text=Thumbnail" />
+          <h6 class="panel">Description</h6>
+        </div>
+ 
+    <!-- End Thumbnails -->
+ 
+      </div>
+    </div>
+  </div>
+ 
+ 
+ 
+  <div class="row">
+    <div class="large-12 columns">
+      <div class="row">
+ 
+    <!-- Content -->
+ 
+        <div class="large-8 columns">
+          <div class="panel radius">
+ 
+          <div class="row">
+          <div class="large-6 small-6 columns">
+ 
+            <h4>Header</h4><hr/>
+            <h5 class="subheader">Risus ligula, aliquam nec fermentum vitae, sollicitudin eget urna. Donec dignissim nibh fermentum odio ornare sagittis.
+            </h5>
+ 
+          <div class="show-for-small" align="center">
+            <a href="#" class="small radius button">Call To Action!</a><br>
+ 
+            <a href="#" class="small radius button">Call To Action!</a>
+          </div>
+ 
+          </div>
+          <div class="large-6 small-6 columns">
+ 
+            <p>Suspendisse ultrices ornare tempor. Aenean eget ultricies libero. Phasellus non ipsum eros. Vivamus at dignissim massa. Aenean dolor libero, blandit quis interdum et, malesuada nec ligula. Nullam erat erat, eleifend sed pulvinar ac. Suspendisse ultrices ornare tempor. Aenean eget ultricies libero.
+          </p>
+        </div>
+ 
+        </div>
+        </div>
+        </div>
+ 
+        <div class="large-4 columns hide-for-small">
+ 
+          <h4>Get In Touch!</h4><hr/>
+ 
+          <a href="#">
+          <div class="panel radius callout" align="center">
+            <strong>Follow me on Facebook</strong>
+          </div>
+          </a>
+ 
+          <a href="#">
+          <div class="panel radius callout" align="center">
+            <strong>Follow me on Twitter</strong>
+          </div>
+          </a>
+ 
+        </div>
+ 
+    <!-- End Content -->
+
+
+    <!-- Footer -->
+
+      <footer class="row">
+        <div class="large-12 columns"> <div class="hr">&nbsp;</div>
+            <div class="row">
+
+              <div class="large-6 columns">
+                  <p>&copy; Copyright no one at all. Go to town.</p>
+              </div>
+
+              <div class="large-6 small-12 columns">
+                  <ul class="inline-list right">
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                    <li><a href="#">Link 4</a></li>
+                  </ul>
+              </div>
+
+            </div>
+        </div>
+      </footer>
+
+    <!-- End Footer -->
+
+    </div>
+  </div>
+
+  <!-- Included JS Files -->
+  <!-- Included JS Files (Uncompressed) -->
+
+  <script src="js/vendor/zepto.js"></script>
+  <script src="js/foundation/foundation.js"></script>
+  <script src="js/foundation/foundation.cookie.js"></script>
+  <script src="js/foundation/foundation.alerts.js"></script>
+  <script src="js/foundation/foundation.clearing.js"></script>
+  <script src="js/foundation/foundation.dropdown.js"></script>
+  <script src="js/foundation/foundation.forms.js"></script>
+  <script src="js/foundation/foundation.joyride.js"></script>
+  <script src="js/foundation/foundation.magellan.js"></script>
+  <script src="js/foundation/foundation.orbit.js"></script>
+  <script src="js/foundation/foundation.placeholder.js"></script>
+  <script src="js/foundation/foundation.reveal.js"></script>
+  <script src="js/foundation/foundation.section.js"></script>
+  <script src="js/foundation/foundation.tooltips.js"></script>
+  <script src="js/foundation/foundation.topbar.js"></script>
+  <!-- Application Javascript, safe to override -->
+
+  <script src="js/foundation/index.js"></script>
+  <script>
+    $(document).foundation().foundation('joyride', 'start');
+  </script>
+  <script>
+  (function($) {
+    
+  var allPanels = $('.accordion > dd').hide();
+    
+  $('.accordion > dt > a').click(function() {
+    allPanels.slideUp();
+    $(this).parent().next().slideDown();
+    return false;
+  });
+
+})(jQuery);
+   </script>
+<!-- End Footer -->
+
+</body>
+</html>
